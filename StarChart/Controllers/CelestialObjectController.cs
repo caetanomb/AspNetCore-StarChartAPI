1df365141908af34a9d5e7752b1e4f10190693a7
caetanomb <caetanomb@gmail.com>
2019-01-02 09:29:00 +0100

    Fifth-a step completed

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -63,7 +63,7 @@ namespace StarChart.Controllers
         [HttpPost]
         public IActionResult Create([FromBody] CelestialObject celestialObject)
         {
-            _context.Add(celestialObject);
+            _context.CelestialObjects.Add(celestialObject);
             _context.SaveChanges();
 
             return CreatedAtRoute("GetById", new { id = celestialObject.Id });
